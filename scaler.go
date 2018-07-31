8bf227ce1e32f13bd1a21b8c6958a1acdd39c170
Justin Lee <justin.r.lee@gmail.com>
2018-07-31 15:07:46 -0400

    Move scaler stuff to scaler.go

--- scaler.go
+++ scaler.go
@@ -1,6 +1,12 @@
 package main
 
+// Scalers - all monitored apps
+// TODO GET RID OF THIS
+type Scalers []Scaler
+
 import (
+	"time"
+
 	log "github.com/Sirupsen/logrus"
 )
 
@@ -9,10 +15,93 @@ type ScaleSignal struct {
 	Scale scaleDirection
 }
 
+// This needs to be turned into an int: -1, 0, 1
 type scaleDirection struct {
 	up   bool
 	down bool
 }
+
+//Scaler struct describing autoscaling policy
+type Scaler struct {
+	AppID        string  `json:"app_id"`
+	MaxCPU       float64 `json:"max_cpu"`
+	MinCPU       float64 `json:"min_cpu"`
+	MaxMem       float64 `json:"max_mem"`
+	MinMem       float64 `json:"min_mem"`
+	Method       string  `json:"method"`
+	ScaleFactor  int     `json:"scale_factor"`
+	MaxInstances int     `json:"max_instances"`
+	MinInstances int     `json:"min_instances"`
+	WarmUp       int     `json:"warm_up"`
+	CoolDown     int     `json:"cool_down"`
+	Interval     int     `json:"interval"`
+}
+
+type scalerState struct {
+	warmUp   int
+	coolDown int
+}
+
+//StartMonitor starts a ticker goroutine
+func (scaler *Scaler) StartMonitor() {
+	tickers[scaler.AppID] = time.NewTicker(time.Second * time.Duration(scaler.Interval))
+	go scaler.doMonitor()
+}
+
+//doMonitor will be storing the intermediate state of the app metrics
+func (scaler *Scaler) doMonitor() {
+	as := scalerState{0, 0}
+	var cpu, mem float64
+	for range tickers[scaler.AppID].C {
+		if !client.AppExists(scaler) {
+			log.Warningf("%s not found in /service/marathon/v2/app", scaler.AppID)
+			continue
+		}
+		marathonApp := client.GetMarathonApp(scaler.AppID)
+		if marathonApp.App.Instances == 0 {
+			log.Warningf("%s suspended, skipping monitoring cycle", marathonApp.App.ID)
+			continue
+		}
+		if !scaler.EnsureMinMaxInstances(marathonApp) {
+			continue
+		}
+		cpu, mem = scaler.getCPUMem(marathonApp)
+		log.Infof("app:%s cpu:%f, mem:%f", scaler.AppID, cpu, mem)
+		scaler.AutoScale(cpu, mem, &as, marathonApp)
+	}
+}
+
+//StopMonitor stops the ticker associated with the given app
+func (scaler *Scaler) StopMonitor() {
+	tickers[scaler.AppID].Stop()
+}
+
+// TODO this doesn't need to be attached to Scaler, I don't think
+func (scaler *Scaler) getCPUMem(marathonApp MarathonApp) (float64, float64) {
+	var (
+		stats1, stats2               TaskStats
+		cpu, cpu1, cpu2, cpuD, timeD float64
+		mem                          float64
+	)
+	marathonApp.FilterNonRunningTasks()
+	for _, task := range marathonApp.App.Tasks {
+		stats1 = client.GetTaskStats(task.ID, task.SlaveID)
+		//TODO: implement a trailing data structure here
+		time.Sleep(time.Second * 1)
+		stats2 = client.GetTaskStats(task.ID, task.SlaveID)
+
+		cpu1 = stats1.Statistics.CpusSystemTimeSecs + stats1.Statistics.CpusUserTimeSecs
+		cpu2 = stats2.Statistics.CpusSystemTimeSecs + stats2.Statistics.CpusUserTimeSecs
+		cpuD = cpu2 - cpu1
+		timeD = stats2.Statistics.Timestamp - stats1.Statistics.Timestamp
+		cpu = cpu + (cpuD / timeD)
+		mem = mem + (stats1.Statistics.MemRssBytes / stats1.Statistics.MemLimitBytes)
+	}
+	cpu = cpu / float64(len(marathonApp.App.Tasks)) * 100
+	mem = mem / float64(len(marathonApp.App.Tasks)) * 100
+	return cpu, mem
+}
+
 
 //generateSignal given cpu and mem values, return a scale proposal
 func generateSignal(cpu, mem float64, scaler *Scaler) ScaleSignal {
